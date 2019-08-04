4a7b3d5f3b8a3d7e10525d90f71eb1489c99c67c
t-tajiri <swallow.ava@gmail.com>
2019-08-04 12:07:13 +0900

    small changes

--- src/main/java/tajiri/example/oauth2example/config/Oauth2AuthorizationServerConfig.java
+++ src/main/java/tajiri/example/oauth2example/config/Oauth2AuthorizationServerConfig.java
@@ -1,6 +1,5 @@
 package tajiri.example.oauth2example.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
