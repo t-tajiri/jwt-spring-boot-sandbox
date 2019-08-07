9e851cd63bd33a9468e7c37244160d95a5fd9840
t-tajiri <swallow.ava@gmail.com>
2019-08-07 22:20:23 +0900

    refactor authentication filter to get external config

--- src/main/java/tajiri/example/oauth2example/config/JWTAuthenticationFilter.java
+++ src/main/java/tajiri/example/oauth2example/config/JWTAuthenticationFilter.java
@@ -1,47 +1,48 @@
 package tajiri.example.oauth2example.config;
 
-import com.auth0.jwt.JWT;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import tajiri.example.oauth2example.model.LoginUser;
+import com.auth0.jwt.*;
+import com.fasterxml.jackson.databind.*;
+import org.springframework.core.env.*;
+import org.springframework.security.authentication.*;
+import org.springframework.security.core.*;
+import org.springframework.security.web.authentication.*;
+import tajiri.example.oauth2example.model.*;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.sql.Date;
-import java.time.Instant;
-import java.util.List;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+import java.sql.*;
+import java.time.*;
 
 import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
+import static tajiri.example.oauth2example.model.Constants.DEFAULT_TOKEN_EXPIRATION_DATE;
+import static tajiri.example.oauth2example.model.Constants.DEFAULT_TOKEN_KEY;
+import static tajiri.example.oauth2example.model.Constants.HEADER_AUTHORIZATION;
+import static tajiri.example.oauth2example.model.Constants.TOKEN_PREFIX;
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
     private AuthenticationManager authenticationManager;
+    private Environment env;
 
-    private List<GrantedAuthority> roles;
-
-    //TODO refactor extract constant
-    private static final String SECRET_KEY = "t-tajiri";
-    private static final String HEADER_AUTHORIZATION = "Authorization";
+    public JWTAuthenticationFilter(AuthenticationManager authenticationManager, Environment env) {
+        this.authenticationManager = authenticationManager;
+        this.env = env;
+    }
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) {
         try {
-            LoginUser loginUser = new ObjectMapper().readValue(request.getInputStream(), LoginUser.class);
+            var loginUser = new ObjectMapper().readValue(request.getInputStream(), LoginUser.class);
 
+            // @formatter:off
             return authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                       loginUser.getUsername()
                     , loginUser.getPassword()
-                    , roles
                 )
             );
+            // @formatter:on
         } catch (IOException e) {
             throw new RuntimeException("parsing request to object when attempt login", e);
         }
@@ -52,13 +53,19 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
                                          HttpServletResponse response,
                                          FilterChain chain,
                                          Authentication auth) throws IOException, ServletException {
+
+        var SECRET_KEY = env.getProperty("TOKEN.SECRET_KEY", DEFAULT_TOKEN_KEY);
+
+        var EXPIRATION_TIME = Long.parseLong(env.getProperty("TOKEN.EXPIRATION_TIME", DEFAULT_TOKEN_EXPIRATION_DATE));
+
+        //TODO extract token manipulation
         // @formatter:off
-        String token = JWT.create()
-                          .withSubject(((LoginUser) auth.getPrincipal()).getUsername())
-                          .withExpiresAt(Date.from(Instant.now()))
-                          .sign(HMAC512(SECRET_KEY.getBytes()));
+        var token = JWT.create()
+                        .withSubject(((LoginUser) auth.getPrincipal()).getUsername())
+                        .withExpiresAt(Date.from(Instant.now().plusMillis(EXPIRATION_TIME)))
+                        .sign(HMAC512(SECRET_KEY.getBytes()));
         // @formatter:on
 
-        response.addHeader(HEADER_AUTHORIZATION, "Bearer " + token);
+        response.addHeader(HEADER_AUTHORIZATION, TOKEN_PREFIX + token);
     }
 }
